570cb3cac2b1cca360dcbd1baf3d7be1b1dc2a80
Mahdihrs <mahdiharis@outlook.com>
2019-02-08 15:52:44 +0700

    translate to indo

--- translate.js
+++ translate.js
@@ -1,10 +1,15 @@
 function google_translate() {
     $.post('http://localhost:3000/translate', {
-        words: $('#coba').text(),
+        words: $('#ingredients').text(),
         lang: 'id'
     })
     .done(response => {
         console.log(response)
+        $('#indoLang').append(`
+            <p>Translated to Bahasa:</p>
+            <p>${response.translatedText}</p>
+        `)
+        $('#ocTranslate').hide()
     })
     .fail(err => {
         console.log(err)
@@ -23,3 +28,4 @@ function getAllLangs() {
 }
 
 // $('#starred').append(`<p>${starred.stargazers_count} stars  <span style="color:black"><strong><a href="${starred.html_url}">View on GitHub</strong></span></p> <hr>`)
+// {translatedText: "1 1/2 cangkir tepung serbaguna, 1 baking soda 1 se…ndok teh bubuk kayu manis, 1 sendok makan mentega", detectedSourceLanguage: "en", originalText: "1 1/2 cups all-purpose flour, 1 teaspoon baking so…1/8 teaspoon ground cinnamon, 1 tablespoon butter"}
